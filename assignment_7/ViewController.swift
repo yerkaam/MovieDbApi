ed9ee585453bc32a2327a5158239be8d4597bf36
yerkaam <eraorynbai@gmail.com>
2024-11-10 16:37:32 +0500

    Initial Commit

---
+++ assignment_7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment_7
+//
+//  Created by Yerdaulet Orynbay on 10.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
